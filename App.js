dc98e282c62822b651be35ca1a3f50715d996fd3
Antoine Yi-Cheng Tian <antoine186@hotmail.com>
2023-03-08 15:49:17 +0100

    Managed to get initial shitty webpage up and running

--- App.js
+++ App.js
@@ -1,20 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Button, StyleSheet, Text, View, Dimensions, TextInput } from 'react-native';
 
-export default function App() {
+function Link(props) {
+  return <Text {...props} accessibilityRole="link" style={StyleSheet.compose(styles.link, props.style)} />;
+}
+
+function App() {
+  const [value, onChangeText] = React.useState('Useless Multiline Placeholder');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.title}>Emotional Machines</Text>
+      </View>
+      <Text style={styles.text}>
+        Search
+      </Text>
+      <Text style={styles.text}>
+        Built with <Link href="https://github.com/facebook/create-react-app">Create React App</Link> and{' '}
+        <Link href="https://github.com/necolas/react-native-web">React Native for Web</Link>
+      </Text>
+      <Button onPress={() => {}} title="Example button" />
+
+      <View class="form-group form-row">
+            <View class="col-10">
+              <Text style={styles.text}>
+                Search
+              </Text>
+              <TextInput
+                editable
+                multiline
+                numberOfLines={4}
+                maxLength={40}
+                onChangeText={text => onChangeText(text)}
+                value={value}
+                style={{padding: 10}}
+              />
+              <label for="from_date" class="col-form-label">
+                  From:
+              </label>
+              <TextInput type="date" id="date" min="2006-01-01"/>
+              <label for="to_date" class="col-form-label">
+                  To:
+              </label>
+              <TextInput type="date" id="maxdatepicker" max="{{date.today()}}"/>
+            </View>
+      </View>
     </View>
   );
 }
 
+let ScreenHeight = Dimensions.get('window').height;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    height: ScreenHeight,
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
   },
+  header: {
+    padding: 20
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: '1.5rem',
+    marginVertical: '1em',
+    textAlign: 'center'
+  },
+  text: {
+    lineHeight: '1.5em',
+    fontSize: '1.125rem',
+    marginVertical: '1em',
+    textAlign: 'center'
+  },
+  link: {
+    color: '#1B95E0'
+  },
+  code: {
+    fontFamily: 'monospace, monospace'
+  }
 });
+export default App;
