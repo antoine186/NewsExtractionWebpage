9f96a00f7d103ba1ffb3f07f2c339499600d9f6b
Antoine Yi-Cheng Tian <antoine186@hotmail.com>
2023-04-03 23:37:56 +0200

    Full redirection between login and search page achieved

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -3,15 +3,18 @@ import { StatusBar } from 'expo-status-bar'
 import { TouchableOpacity, Text, View, Image, TextInput } from 'react-native'
 import styles from '../utils/style_guide/LoginPageStyle'
 import { api, loginAuthUrl } from '../utils/backend_configuration/BackendConfig'
-import { useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { validateUserSession } from '../store/Slices/UserSessionSlice'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Navigate } from 'react-router-dom'
 
 function Login () {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const dispatch = useDispatch()
   const navigate = useNavigate()
+
+  // const [userSessionValidated, setUserSessionValidation] = useState(this.props.userSession.validated)
+  const userSessionValidated = useSelector(state => state.userSession.validated)
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -31,36 +34,40 @@ function Login () {
     )
   }
 
-  return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={require('../assets/images/EMOfficialLogo.png')} />
-      <Text style={styles.company_name}>Emotional Machines</Text>
-      <StatusBar style="auto" />
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Email"
-          placeholderTextColor="#003f5c"
-          onChangeText={(email) => setUsername(email)}
-        />
+  if (userSessionValidated) {
+    return <Navigate to='/' />
+  } else {
+    return (
+      <View style={styles.container}>
+        <Image style={styles.image} source={require('../assets/images/EMOfficialLogo.png')} />
+        <Text style={styles.company_name}>Emotional Machines</Text>
+        <StatusBar style="auto" />
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Email"
+            placeholderTextColor="#003f5c"
+            onChangeText={(email) => setUsername(email)}
+          />
+        </View>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Password"
+            placeholderTextColor="#003f5c"
+            secureTextEntry={true}
+            onChangeText={(password) => setPassword(password)}
+          />
+        </View>
+        <TouchableOpacity>
+          <Text style={styles.forgot_button}>Forgot Password?</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.loginBtn} onPress={handleSubmit}>
+          <Text style={styles.loginText}>LOGIN</Text>
+        </TouchableOpacity>
       </View>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Password"
-          placeholderTextColor="#003f5c"
-          secureTextEntry={true}
-          onChangeText={(password) => setPassword(password)}
-        />
-      </View>
-      <TouchableOpacity>
-        <Text style={styles.forgot_button}>Forgot Password?</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.loginBtn} onPress={handleSubmit}>
-        <Text style={styles.loginText}>LOGIN</Text>
-      </TouchableOpacity>
-    </View>
-  )
+    )
+  }
 }
 
 export default Login
