1277ade96f5496f617a47efa8e1ec7af0573b509
Antoine Yi-Cheng Tian <antoine186@hotmail.com>
2023-05-06 20:50:32 +0200

    Fixed incorrect emo percentage change calculation

--- src/containers/search_helper_functions/EmoChangeStringFormatter.jsx
+++ src/containers/search_helper_functions/EmoChangeStringFormatter.jsx
@@ -12,46 +12,46 @@ function EmoChangeStringFormatter (emoBreakdown, previousEmoBreakdown) {
   })
 
   let emoSign1 = ''
-  if (((previousEmoBreakdown[emoBreakdownSorted[0][0]] - emoBreakdownSorted[0][1]) / emoBreakdownSorted[0][1] * 100) >= 0) {
+  if (((emoBreakdownSorted[0][1] - previousEmoBreakdown[emoBreakdownSorted[0][0]]) / previousEmoBreakdown[emoBreakdownSorted[0][0]] * 100) >= 0) {
     emoSign1 = '+'
   }
-  emoEngagementFormattedString = emoIcons[emoBreakdownSorted[0][0]] + ' ' + emoSign1 + ((previousEmoBreakdown[emoBreakdownSorted[0][0]] - emoBreakdownSorted[0][1]) / emoBreakdownSorted[0][1] * 100).toFixed(2) + '% '
+  emoEngagementFormattedString = emoIcons[emoBreakdownSorted[0][0]] + ' ' + emoSign1 + ((emoBreakdownSorted[0][1] - previousEmoBreakdown[emoBreakdownSorted[0][0]]) / previousEmoBreakdown[emoBreakdownSorted[0][0]] * 100).toFixed(2) + '% '
 
   let emoSign2 = ''
-  if (((previousEmoBreakdown[emoBreakdownSorted[1][0]] - emoBreakdownSorted[1][1]) / emoBreakdownSorted[1][1] * 100) >= 0) {
+  if (((emoBreakdownSorted[1][1] - previousEmoBreakdown[emoBreakdownSorted[1][0]]) / previousEmoBreakdown[emoBreakdownSorted[1][0]] * 100) >= 0) {
     emoSign2 = '+'
   }
-  emoEngagementFormattedString = emoEngagementFormattedString + emoIcons[emoBreakdownSorted[1][0]] + ' ' + emoSign2 + ((previousEmoBreakdown[emoBreakdownSorted[1][0]] - emoBreakdownSorted[1][1]) / emoBreakdownSorted[1][1] * 100).toFixed(2) + '% '
+  emoEngagementFormattedString = emoEngagementFormattedString + emoIcons[emoBreakdownSorted[1][0]] + ' ' + emoSign2 + ((emoBreakdownSorted[1][1] - previousEmoBreakdown[emoBreakdownSorted[1][0]]) / previousEmoBreakdown[emoBreakdownSorted[1][0]] * 100).toFixed(2) + '% '
 
   let emoSign3 = ''
-  if (((previousEmoBreakdown[emoBreakdownSorted[2][0]] - emoBreakdownSorted[2][1]) / emoBreakdownSorted[2][1] * 100) >= 0) {
+  if (((emoBreakdownSorted[2][1] - previousEmoBreakdown[emoBreakdownSorted[2][0]]) / previousEmoBreakdown[emoBreakdownSorted[2][0]] * 100) >= 0) {
     emoSign3 = '+'
   }
-  emoEngagementFormattedString = emoEngagementFormattedString + emoIcons[emoBreakdownSorted[2][0]] + ' ' + emoSign3 + ((previousEmoBreakdown[emoBreakdownSorted[2][0]] - emoBreakdownSorted[2][1]) / emoBreakdownSorted[2][1] * 100).toFixed(2) + '% '
+  emoEngagementFormattedString = emoEngagementFormattedString + emoIcons[emoBreakdownSorted[2][0]] + ' ' + emoSign3 + ((emoBreakdownSorted[2][1] - previousEmoBreakdown[emoBreakdownSorted[2][0]]) / previousEmoBreakdown[emoBreakdownSorted[2][0]] * 100).toFixed(2) + '% '
 
   let emoSign4 = ''
-  if (((previousEmoBreakdown[emoBreakdownSorted[3][0]] - emoBreakdownSorted[3][1]) / emoBreakdownSorted[3][1] * 100) >= 0) {
+  if (((emoBreakdownSorted[3][1] - previousEmoBreakdown[emoBreakdownSorted[3][0]]) / previousEmoBreakdown[emoBreakdownSorted[3][0]] * 100) >= 0) {
     emoSign4 = '+'
   }
-  emoEngagementFormattedString = emoEngagementFormattedString + emoIcons[emoBreakdownSorted[3][0]] + ' ' + emoSign4 + ((previousEmoBreakdown[emoBreakdownSorted[3][0]] - emoBreakdownSorted[3][1]) / emoBreakdownSorted[3][1] * 100).toFixed(2) + '% '
+  emoEngagementFormattedString = emoEngagementFormattedString + emoIcons[emoBreakdownSorted[3][0]] + ' ' + emoSign4 + ((emoBreakdownSorted[3][1] - previousEmoBreakdown[emoBreakdownSorted[3][0]]) / previousEmoBreakdown[emoBreakdownSorted[3][0]] * 100).toFixed(2) + '% '
 
   let emoSign5 = ''
-  if (((previousEmoBreakdown[emoBreakdownSorted[4][0]] - emoBreakdownSorted[4][1]) / emoBreakdownSorted[4][1] * 100) >= 0) {
+  if (((emoBreakdownSorted[4][1] - previousEmoBreakdown[emoBreakdownSorted[4][0]]) / previousEmoBreakdown[emoBreakdownSorted[4][0]] * 100) >= 0) {
     emoSign5 = '+'
   }
-  emoEngagementFormattedString = emoEngagementFormattedString + emoIcons[emoBreakdownSorted[4][0]] + ' ' + emoSign5 + ((previousEmoBreakdown[emoBreakdownSorted[4][0]] - emoBreakdownSorted[4][1]) / emoBreakdownSorted[4][1] * 100).toFixed(2) + '% '
+  emoEngagementFormattedString = emoEngagementFormattedString + emoIcons[emoBreakdownSorted[4][0]] + ' ' + emoSign5 + ((emoBreakdownSorted[4][1]) - previousEmoBreakdown[emoBreakdownSorted[4][0]] / previousEmoBreakdown[emoBreakdownSorted[4][0]] * 100).toFixed(2) + '% '
 
   let emoSign6 = ''
-  if (((previousEmoBreakdown[emoBreakdownSorted[5][0]] - emoBreakdownSorted[5][1]) / emoBreakdownSorted[5][1] * 100) >= 0) {
+  if (((emoBreakdownSorted[5][1] - previousEmoBreakdown[emoBreakdownSorted[5][0]]) / previousEmoBreakdown[emoBreakdownSorted[5][0]] * 100) >= 0) {
     emoSign6 = '+'
   }
-  emoEngagementFormattedString = emoEngagementFormattedString + emoIcons[emoBreakdownSorted[5][0]] + ' ' + emoSign6 + ((previousEmoBreakdown[emoBreakdownSorted[5][0]] - emoBreakdownSorted[5][1]) / emoBreakdownSorted[5][1] * 100).toFixed(2) + '% '
+  emoEngagementFormattedString = emoEngagementFormattedString + emoIcons[emoBreakdownSorted[5][0]] + ' ' + emoSign6 + ((emoBreakdownSorted[5][1] - previousEmoBreakdown[emoBreakdownSorted[5][0]]) / previousEmoBreakdown[emoBreakdownSorted[5][0]] * 100).toFixed(2) + '% '
 
   let emoSign7 = ''
-  if (((previousEmoBreakdown[emoBreakdownSorted[6][0]] - emoBreakdownSorted[6][1]) / emoBreakdownSorted[6][1] * 100) >= 0) {
+  if (((emoBreakdownSorted[6][1] - previousEmoBreakdown[emoBreakdownSorted[6][0]]) / previousEmoBreakdown[emoBreakdownSorted[6][0]] * 100) >= 0) {
     emoSign7 = '+'
   }
-  emoEngagementFormattedString = emoEngagementFormattedString + emoIcons[emoBreakdownSorted[6][0]] + ' ' + emoSign7 + ((previousEmoBreakdown[emoBreakdownSorted[6][0]] - emoBreakdownSorted[6][1]) / emoBreakdownSorted[6][1] * 100).toFixed(2) + '% '
+  emoEngagementFormattedString = emoEngagementFormattedString + emoIcons[emoBreakdownSorted[6][0]] + ' ' + emoSign7 + ((emoBreakdownSorted[6][1] - previousEmoBreakdown[emoBreakdownSorted[6][0]]) / previousEmoBreakdown[emoBreakdownSorted[6][0]] * 100).toFixed(2) + '% '
 
   return emoEngagementFormattedString
 }
